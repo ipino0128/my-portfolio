088c1171784a1728a97ce7b2171df0febb16e98d
github username <github email address>
2019-03-10 19:28:19 -0400

    background styling

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return(
+    <div className="Header">
+      <div className="name">
+        Isabelle Pino
+      </div>
+    </div>
+
+  )
+
+}
+export default Header
