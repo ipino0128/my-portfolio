f630fc3a5595ea382853bb45bd250e4c4f79cf18
github username <github email address>
2019-03-10 20:22:56 -0400

    buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return(
-    <div className="Header">
-      <div className="name">
-        Isabelle Pino
-      </div>
+    <div className="container">
+        <img src="https://image.freepik.com/free-photo/close-up-hand-typing-laptop-with-stationeries-coffee-cup-pink-backdrop_23-2147979078.jpg" />
+        <div className="name">
+          I S A B E L L E <br/> P I N O
+        </div>
+        <button type="button" class="btn btn-outline-dark">About</button>
     </div>
+
 
   )
 
